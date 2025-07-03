3885b6b1849b3008ff0a31d8785cf4ff951c9696
tejas-shivanand-mane <tejas.shivanand.mane@gmail.com>
2025-07-03 09:26:31 -0700

    test

--- src/consensus.cpp
+++ src/consensus.cpp
@@ -371,7 +371,7 @@ void HotStuffCore::on_receive_commit1(const Commit1 &vote) {
         sent_prepares.clear();
 //        HOTSTUFF_LOG_INFO("After clearing sent_prepares size: %d", sent_prepares.size());
         LOG_INFO("blk->get_height(), part_decided is %d, %d", blk->get_height(), get_part_decided());
-        if (get_part_decided() < 50000 || get_part_decided() > 54000)
+        if (get_part_decided() < 40000 || get_part_decided() > 44000)
         {
             on_commit(blk);
         }
